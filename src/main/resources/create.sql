7d86c1930b8feea31245a69c58f68fa2c2361b5a
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-11-30 12:14:45 -0300

    adicionado a create table o dth_fim

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -119,14 +119,16 @@ create table jogador_cartaobjetivo (
      REFERENCES carta_objetivo (id),
      CONSTRAINT FK_jogador_id FOREIGN KEY (jogador_id)
      REFERENCES jogador (id));
-    
+
 create table sala (
-    id uuid not null, 
-    hash varchar(255) not null,
+     id uuid not null, 
+     hash varchar(255) not null,
      status int4 not null,
      baralho_id uuid, 
      dado int4 not null,
-     primary key (id));
+     dth_fim timestamp,
+     primary key (id)
+);
 
 create table sala_jogadores (
     sala_id uuid not null,
