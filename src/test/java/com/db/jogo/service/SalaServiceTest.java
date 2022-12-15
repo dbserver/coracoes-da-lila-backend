8e00e495ad913f1951f4901aabe87056d5cc4c46
Rayane Paiva <rayanepaiiva@live.com>
2022-12-15 14:00:57 -0300

    refactor: alteração de nome de variaveis e métodos.

--- src/test/java/com/db/jogo/service/SalaServiceTest.java
+++ src/test/java/com/db/jogo/service/SalaServiceTest.java
@@ -61,9 +61,9 @@ public class SalaServiceTest {
         carta.setTipo("Ação");
 
         cartaObjetivo.setId(UUID.randomUUID());
-        cartaObjetivo.setDescricao("Texto da carta");
+        cartaObjetivo.setTexto_tematico("Texto da carta");
         cartaObjetivo.setPontos(0);
-        cartaObjetivo.setClassificacao("Ganhe pontos");
+        cartaObjetivo.setTexto_regra("Ganhe pontos");
         cartaObjetivo.setCategoria("Física");
 
         baralho.setId(UUID.randomUUID());
