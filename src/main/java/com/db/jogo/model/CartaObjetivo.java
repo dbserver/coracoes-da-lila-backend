aae0ec2621acf6807b56a7ca98c192920f98d1fc
Rayane Paiva <rayanepaiiva@live.com>
2022-12-16 12:42:58 -0300

    fix: remoção de import desnecessário

--- src/main/java/com/db/jogo/model/CartaObjetivo.java
+++ src/main/java/com/db/jogo/model/CartaObjetivo.java
@@ -9,8 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.CollectionId;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
