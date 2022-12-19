d9b8a631a5797aaa5fd2a69ac2f3763c8dd8a01c
Rayane Paiva <rayanepaiiva@live.com>
2022-12-19 13:10:01 -0300

    refactor: colunas categorias e tipo podem ser nulas

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -39,11 +39,11 @@ create table baralho (
 create table carta_do_jogo (
      id uuid primary key  not null,
      bonus boolean not null, 
-     categoria varchar(80) not null,
+     categoria varchar(80),
      fonte varchar(80) not null, 
      pontos int4 not null,
      texto varchar(255) not null, 
-     tipo varchar(50) not null, 
+     tipo varchar(50), 
      valor_cor_grande int4,
      valor_cor_pequeno int4
              
