0a7eb7a7f80cd21229430af0b41901a80fe24f17
Rayane Paiva <rayanepaiiva@live.com>
2022-12-19 13:18:18 -0300

    fix: colunas não podem ser nulas

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -39,11 +39,11 @@ create table baralho (
 create table carta_do_jogo (
      id uuid primary key  not null,
      bonus boolean not null, 
-     categoria varchar(80),
+     categoria varchar(80) not null,
      fonte varchar(80) not null, 
      pontos int4 not null,
      texto varchar(255) not null, 
-     tipo varchar(50), 
+     tipo varchar(50) not null, 
      valor_cor_grande int4,
      valor_cor_pequeno int4
              
