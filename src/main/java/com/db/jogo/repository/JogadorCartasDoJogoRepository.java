65371452923e0ea1536eff29b041ae831461ca63
Rayane Paiva <rayanepaiiva@live.com>
2022-12-29 12:32:15 -0300

    refactor: remoção import desnecessario

--- src/main/java/com/db/jogo/repository/JogadorCartasDoJogoRepository.java
+++ src/main/java/com/db/jogo/repository/JogadorCartasDoJogoRepository.java
@@ -3,12 +3,9 @@ package com.db.jogo.repository;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.db.jogo.model.JogadorCartasDoJogo;
 
-@Repository
-public interface JogadorCartasDoJogoRepository extends CrudRepository<JogadorCartasDoJogo, UUID>{
-    //JogadorCartasDoJogo findByIdCartaDoJogo(UUID id);
+public interface JogadorCartasDoJogoRepository extends CrudRepository<JogadorCartasDoJogo, UUID>{    
     JogadorCartasDoJogo findByJogadorIDAndCartaDoJogoID(UUID jogadorID, UUID cartaID);
 }
